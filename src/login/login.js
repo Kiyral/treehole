99a8a4cf6887b9a5c3c842a92a42be0cc001efee
Swelldg <mike_1018@126.com>
2023-04-26 15:09:41 +0800

    modify ugly css

--- src/login/login.js
+++ src/login/login.js
@@ -1,106 +1,121 @@
-import { LockOutlined, UserOutlined, CloseOutlined} from '@ant-design/icons';
-import { Button, Form, Input } from 'antd';
-import axios from 'axios'
-import './login.css'
+import { LockOutlined, UserOutlined, CloseOutlined } from "@ant-design/icons";
+import { Button, Form, Input } from "antd";
+import axios from "axios";
+import "./login.css";
 const formItemLayout = {
-    labelcol: {
-      xs: {
-        span: 24,
-      },
-      sm: {
-        span: 8,
-      },
+  labelcol: {
+    xs: {
+      span: 24,
     },
-    wrapperCol: {
-      xs: {
-        span: 24,
-      },
-      sm: {
-        span: 16,
-      },
+    sm: {
+      span: 8,
     },
-  };
-  const tailFormItemLayout = {
-    wrappercol: {
-      xs: {
-        span: 24,
-        offset: 0,
-      },
-      sm: {
-        span: 16,
-        offset: 8,
-      },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
     },
-  };
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrappercol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
 const Login = (props) => {
   const onFinish = (values) => {
     const data = {
-        email: values.email,
-        password: values.password,
-      };
-      
-      const headers = {
-        'Content-Type': 'application/json'
-      };
-      
-      axios.post('http://localhost:3000/api/login', data, { headers })
-        .then(response => {
-            console.log(response.data);
-            alert('登陆成功');
-            props.onClose();
-        })
-        .catch(error => {
-            console.error(error);
-            alert(error.response.data.message);
-        });
+      email: values.email,
+      password: values.password,
+    };
+
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    axios
+      .post("http://localhost:3000/api/login", data, { headers })
+      .then((response) => {
+        console.log(response.data);
+        alert("登陆成功");
+        props.onClose();
+      })
+      .catch((error) => {
+        console.error(error);
+        alert(error.response.data.message);
+      });
   };
   return (
-    <div className='background'>
-    <div className='closebutton'><Button type="primary" shape="circle" icon={<CloseOutlined />} size="medium" block='true' onClick={props.onClose}/></div>
-    <Form
-    {...formItemLayout}
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      size='large'
-    >
-      <Form.Item
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: '请输入你的邮箱!',
-          },
-        ]}
-      >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="邮箱" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: '请输入你的密码!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="密码"
+    <div className="background">
+      <div className="closebutton">
+        <Button
+          type="primary"
+          shape="circle"
+          icon={<CloseOutlined />}
+          size="medium"
+          block="true"
+          onClick={props.onClose}
         />
-      </Form.Item>
+      </div>
+      <Form
+        {...formItemLayout}
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        size="large"
+      >
+        <Form.Item
+          className="login-form-label"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "请输入你的邮箱!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="邮箱"
+          />
+        </Form.Item>
+        <Form.Item
+          className="login-form-label"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "请输入你的密码!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="密码"
+          />
+        </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button" {...tailFormItemLayout}>
-          登录
-        </Button>
-        <a href="#" onClick={props.goRegister}>现在注册!</a>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <div className="login-form-button">登录</div>
+          <div className="sign-form-button" onClick={props.goRegister}>
+            现在注册!
+          </div>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
