66cb31476265466689530819c6995ee5e2c5e0dc
kiyral <2226759276@qq.com>
2023-04-28 13:43:28 +0800

    有登录

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import Register from "./register/register";
-import Login from "./login/login";
 import "./App.css";
 import Home from "./pages/home/home";
 import Title from "./components/title/title";
@@ -9,42 +6,6 @@ import { ConfigProvider } from "antd";
 
 
 function App() {
-  const [showLogin, setShowLogin] = useState(false);
-  const [showRegister, setShowRegister] = useState(false);
-
-  const handleLoginClick = () => {
-    setShowLogin(true);
-  };
-
-  const handleCloseLogin = () => {
-    setShowLogin(false);
-    setShowRegister(false);
-  };
-
-  const handleGoRegister = () => {
-    setShowLogin(false);
-    setShowRegister(true);
-  };
-
-  useEffect(() => {
-    // 从 localStorage 中获取 token
-    const token = localStorage.getItem('token');
-
-    // 发送请求时添加 Authorization 头
-    axios.get('http://localhost:3000/api/profile', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-
   return (
     <ConfigProvider
       theme={{
@@ -56,18 +17,6 @@ function App() {
       <div>
         <Title />
         <Home />
-        {/*<h1>Home Page</h1>*/}
-        {/*<button onClick={handleLoginClick}>Login</button>*/}
-        {/*{showLogin && (*/}
-        {/*  <div className="overlay">*/}
-        {/*    <Login onClose={handleCloseLogin} goRegister={handleGoRegister} />*/}
-        {/*  </div>*/}
-        {/*)}*/}
-        {/*{showRegister && (*/}
-        {/*  <div className="overlay">*/}
-        {/*    <Register onClose={handleCloseLogin} />*/}
-        {/*  </div>*/}
-        {/*)}*/}
       </div>
     </ConfigProvider>
   );
--- src/components/title/title.js
+++ src/components/title/title.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./title.css";
 import { Input } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
+import Register from "../register/register";
+import Login from "../login/login";
 
 function Title() {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+
+  const handleLoginClick = () => {
+    setShowLogin(true);
+  };
+
+  const handleCloseLogin = () => {
+    setShowLogin(false);
+    setShowRegister(false);
+  };
+
+  const handleGoRegister = () => {
+    setShowLogin(false);
+    setShowRegister(true);
+  };
+
+  useEffect(() => {
+    // 从 localStorage 中获取 token
+    const token = localStorage.getItem('token');
+
+    // 发送请求时添加 Authorization 头
+    axios.get('http://localhost:3000/api/profile', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
   return (
     <div className="title-wrapper">
       <div style={{ display: "flex" }}>
@@ -19,8 +56,18 @@ function Title() {
         </div>
       </div>
       <div className="title-action">
-        <div className="title-login">登录</div>
-        <div className="title-sign">注册</div>
+        <div className="title-login" onClick={handleLoginClick}>登录</div>
+        {showLogin && (
+          <div className="overlay">
+            <Login onClose={handleCloseLogin} goRegister={handleGoRegister} />
+        </div>
+  )}
+        <div className="title-sign" onClick={handleGoRegister}>注册</div>
+        {showRegister && (
+          <div className="overlay">
+          <Register onClose={handleCloseLogin} />
+          </div>
+        )}
       </div>
     </div>
   );
